72e9f5aafd5fbb8dca72901293dc7d23830662a3
DFife99 <Devlan.fife2@gmail.com>
2021-01-08 13:50:45 +0000

    changed styling of the hover on products page

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,8 @@
 
 $( document ).ready(function() {
     $(".product-display-box").hover( function() {
-        $(this).removeClass("col-3 ml-5 mr-4 mt-5 mb-5").addClass("col-4 ml-0 mr-0 mt-1 mb-1");
+        $(this).removeClass("col-3 ml-5 mr-4 mt-5 mb-5").addClass("col-4 ml-0 mr-0 mt-4 mb-4");
     }, function () {
-        $(this).removeClass("col-4 ml-0 mr-0 mt-1 mb-1").addClass("col-3 ml-5 mr-4 mt-5 mb-5");
+        $(this).removeClass("col-4 ml-0 mr-0 mt-4 mb-4").addClass("col-3 ml-5 mr-4 mt-5 mb-5");
     });
 });
