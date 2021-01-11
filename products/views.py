71a5aa951f16a059ca851aea436c30956272d7c2
DFife99 <Devlan.fife2@gmail.com>
2021-01-11 20:02:19 +0000

    added sorting by category, price and release, to the nav bar

--- products/views.py
+++ products/views.py
@@ -12,11 +12,46 @@ def all_products(request):
     """ a view to return all_products.html, including sorting and searching """
 
     products = Product.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            print(sortkey)
+
+    if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
 
     if request.GET:
         if 'brand' in request.GET:
             brands = request.GET['brand'].split(',')
             products = products.filter(brand__in=brands)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(brand__icontains=query) | Q(category__name__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
