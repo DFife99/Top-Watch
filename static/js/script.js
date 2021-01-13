1201dbf230f3fafc4a2c83bb583fc4625024e677
DFife99 <Devlan.fife2@gmail.com>
2021-01-13 20:52:31 +0000

    added sorting for products page

--- static/js/script.js
+++ static/js/script.js
@@ -16,3 +16,26 @@ $(document).ready(function() {
         $(this).removeClass("col-md-6 col-lg-4 product-margin-hover").addClass("col-md-5 col-lg-3 product-margin");
     });
 });
+
+$(document).ready(function() {
+    $('#sort-selector').change(function() {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedValue = selector.val();
+    if(selectedValue != 'reset'){
+        var sort = selectedValue.split('_')[0];
+        var direction = selectedValue.split('_')[1];
+
+        currentUrl.searchParams.set('sort', sort);
+        currentUrl.searchParams.set('direction', direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete('sort');
+        currentUrl.searchParams.delete('direction');
+
+        window.location.replace(currentUrl)
+    }
+})
+})
