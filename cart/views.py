c4fd5730b9b7b9e0151d498b7702cb98614c7228
dfife99 <devlanfife2@gmail.com>
2021-04-08 18:55:16 +0100

    bug fixed adding to cart

--- cart/views.py
+++ cart/views.py
@@ -17,7 +17,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     # Taking variables from the html form
     redirect_url = request.POST.get('redirect_url')
-    quantity = (request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
 
     item_id = request.POST.get('product-id')
 
@@ -30,11 +30,9 @@ def add_to_cart(request, item_id):
 
     # Checking to see if the ITEM ID is already in the cart
     if item_id in list(cart.keys()):
-
         # Checking to see if the COLOUR is already in the cart
         # under a product with the same ID
-        if colour in list(cart[item_id]):
-
+        if colour in list(cart[item_id]['products_by_colour']):
             # Checking to see if the STROAGE is already in the cart
             # under a product with the same ID and COLOUR
             if storage in list(
@@ -52,14 +50,12 @@ def add_to_cart(request, item_id):
                     {storage: quantity})
 
         else:
-
             # If the colour is different to those already in the cart
             # it will create another colour key and include the storage
             # and the quantity of 1
             cart[item_id]['products_by_colour'].update(
                 {colour: {'products_by_size': {storage: quantity}}})
     else:
-
         # If the item id isnt in the cart at all, it will create a new
         # Dictionary for the new item
         cart[item_id] = {
